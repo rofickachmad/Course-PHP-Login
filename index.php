ba804a615c1f640f0166ec3065ab25244671ff96
muslikhahmad <ftistudent@student.unmer.ac.id>
2024-04-24 13:02:17 +0700

    Perubahan halaman index

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="robots" content="follow">
 
-    <title>Page Title</title>
+    <title>Halaman Index</title>
 
     <base href="/" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
